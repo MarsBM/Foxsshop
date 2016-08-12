88ca40fc1f68a1a6f9f936f9d6be70969be20a76
MarsBM <maryan.babij@gmail.com>
2016-08-12 16:54:52 +0300

    Успішний запуск

--- src/main/java/domain/Customer.java
+++ src/main/java/domain/Customer.java
@@ -1,18 +1,45 @@
 package domain;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.Date;
 import java.util.List;
 
 /**
  * Created by Mars on 12.08.2016.
  */
+@Entity
+@Table(name = "Customer")
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "firstName", length = 25)
+    @NotNull
+    @Size(min = 1, max = 25)
     private String firstName;
+
+    @Column(name = "lastName", length = 25)
+    @NotNull
+    @Size(min = 1, max = 25)
     private String lastName;
+
+    @Column(name = "email", unique = true)
+    @Pattern(regexp = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}$")
     private String email;
-    private List<String> telephone;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Telephone> telephones;
+
+    @Column(name = "password")
+    @NotNull
     private String password;
+
     private String cart;
     private String wishlist;
     private Boolean newsletter;
@@ -22,4 +49,47 @@ public class Customer {
     private Boolean isEnabled;
     private Date createDate;
     private Date modifyDate;
+
+    public Customer() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public List<Telephone> getTelephones() {
+        return telephones;
+    }
+
+    public void setTelephones(List<Telephone> telephones) {
+        this.telephones = telephones;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/main/java/domain/CustomerGroup.java
+++ src/main/java/domain/CustomerGroup.java
@@ -1,9 +1,43 @@
 package domain;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
 /**
  * Created by Mars on 12.08.2016.
  */
+@Entity
+@Table(name = "CustomerGroup")
 public class CustomerGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "name", unique = true, length = 25)
+    @NotNull
+    @Size(min = 2, max = 25)
     private String name;
+
+    public CustomerGroup() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
